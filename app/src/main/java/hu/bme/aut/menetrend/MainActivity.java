e2c1a272b3599cbf20c60edae726f68c4868a497
Sallai Norbert <lamanoru@icloud.com>
2025-11-29 18:08:54 +0100

    projekt indulása
    első lépések

---
+++ app/src/main/java/hu/bme/aut/menetrend/MainActivity.java
@@ -0,0 +1,12 @@
+package hu.bme.aut.menetrend;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
