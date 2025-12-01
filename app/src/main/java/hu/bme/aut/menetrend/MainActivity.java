86a6e4089ca74c8047c1a17d583418a99cb12725
Sallai Norbert <lamanoru@icloud.com>
2025-12-01 21:03:11 +0100

    képernyők és a menü hozzáadva

--- app/src/main/java/hu/bme/aut/menetrend/MainActivity.java
+++ app/src/main/java/hu/bme/aut/menetrend/MainActivity.java
@@ -1,12 +1,104 @@
 package hu.bme.aut.menetrend;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+import hu.bme.aut.menetrend.ui.about.AboutFragment;
+import hu.bme.aut.menetrend.ui.detail.RouteDetailFragment;
+import hu.bme.aut.menetrend.ui.favorites.FavoritesFragment;
+import hu.bme.aut.menetrend.ui.results.ResultsFragment;
+import hu.bme.aut.menetrend.ui.search.SearchFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+    private MaterialToolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+            showFragment(new SearchFragment());
+            navView.setCheckedItem(R.id.nav_search);
+        }
+    }
+
+    private void showFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    public void openResultsFragment(String from, String to, int maxTransfers) {
+        Fragment fragment = ResultsFragment.newInstance(from, to, maxTransfers);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    public void openRouteDetailFragment(long routeId) {
+        Fragment fragment = RouteDetailFragment.newInstance(routeId);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_search) {
+            showFragment(new SearchFragment());
+        } else if (id == R.id.nav_favorites) {
+            showFragment(new FavoritesFragment());
+        } else if (id == R.id.nav_about) {
+            showFragment(new AboutFragment());
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
